55a9e3df8543ba89322e7fa58aed6dcc78aa4b51
Elendan <arthur.despeaux@hotmail.fr>
2017-11-28 10:49:23 +0100

    greetings

--- src/dialogs/GreetingsDialog.ts
+++ src/dialogs/GreetingsDialog.ts
@@ -1,6 +1,8 @@
 import * as builder from "botbuilder";
 import BaseDialog from "./basedialog";
 import MessagesController from "../controllers/MessagesController";
+import MessageTypes from "../enums/MessageTypes";
+import { Message } from "botbuilder";
 
 class GreetingsDialog extends BaseDialog {
     constructor() {
@@ -13,7 +15,30 @@ class GreetingsDialog extends BaseDialog {
                 quickRepliesCard.text("You can find products using the buttons below or simply typing the name of the product.");
                 quickRepliesCard = MessagesController.addQuickRepliesButtons(quickRepliesCard, quickRepliesButtons, "Brands 🍾");
                 quickRepliesCard = MessagesController.addQuickRepliesButtons(quickRepliesCard, quickRepliesButtons, "Categories 🍸");
-                session.send(MessagesController.sendQuickReplies(session, quickRepliesCard));
+                if (session.message.source === "facebook") {
+                    let facebookMessage = new builder.Message(session);
+                    facebookMessage.sourceEvent({
+                        "messages": [
+                            {
+                                "type": MessageTypes.Text,
+                                "speech": "Hello and welcome in the Pernod Ricard's catalog of products.",
+                                "platform": "facebook"
+                            },
+                            {
+                                "type": MessageTypes.QuickReplies,
+                                "title": "You can find products using the buttons below or simply typing the name of the product.",
+                                "replies": [
+                                    "Brands 🍾",
+                                    "Categories 🍸"
+                                ],
+                                "platform": "facebook"
+                            }
+                        ]
+                    });
+                }
+                else {
+                    session.send(MessagesController.sendQuickReplies(session, quickRepliesCard));
+                }
                 session.endDialog();
             }
         ]
