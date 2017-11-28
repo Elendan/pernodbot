63925e8bf52df3e4e8a870b5463c8fe3aea8b298
Elendan <arthur.despeaux@hotmail.fr>
2017-11-28 16:01:46 +0100

    product info

--- src/dialogs/ProductInfoDialog.ts
+++ src/dialogs/ProductInfoDialog.ts
@@ -9,43 +9,76 @@ class ProductInfoDialog extends BaseDialog {
             (session, args, next) => {
                 let quickRepliesCard = new builder.HeroCard(session);
                 let quickRepliesButtons: builder.ICardAction[] = [];
+                let messageSent: string;
                 switch (args.intent.matched[0]) {
                     case "Size":
                         if (!session.userData.productSize) {
                             session.send("None");
                             break;
                         }
-                        session.send(`The bottle's size is ${session.userData.productSize}`);
+                        messageSent = `The bottle's size is ${session.userData.productSize}`;
                         break;
                     case "Ingredients":
                         if (!session.userData.productIngredients) {
                             session.send("None");
                             break;
                         }
-                        session.send(session.userData.productIngredients);
+                        messageSent = session.userData.productIngredients;
                         break;
                     case "Consumption Tips":
                         if (!session.userData.consumptionTips) {
                             session.send("None");
                             break;
                         }
-                        session.send(session.userData.consumptionTips);
+                        messageSent = session.userData.consumptionTips;
                         break;
                     case "Product History":
                         if (!session.userData.productHistory) {
                             session.send("None");
                             break;
                         }
-                        session.send(session.userData.productHistory);
+                        messageSent = session.userData.productHistory;
                         break;
                     default:
                         session.send("Not found");
                         break;
                 }
-                quickRepliesCard = MessagesController.addQuickRepliesButtons(quickRepliesCard, quickRepliesButtons, "Buy this product 🛒");
-                quickRepliesCard = MessagesController.addQuickRepliesButtons(quickRepliesCard, quickRepliesButtons, "More Details");
-                quickRepliesCard = MessagesController.addQuickRepliesButtons(quickRepliesCard, quickRepliesButtons);
-                session.send(MessagesController.sendQuickReplies(session, quickRepliesCard, "What do you want to do ?"));
+                switch (session.message.source) {
+                    case "facebook":
+                        let facebookMessage = new builder.Message(session).text(messageSent);
+                        session.send(facebookMessage);
+                        facebookMessage.text("What do you want to do ?");
+                        facebookMessage.sourceEvent({
+                            facebook: {
+                                quick_replies: [
+                                    {
+                                        content_type: "text",
+                                        title: "Buy this product 🛒",
+                                        payload: "Buy this product 🛒"
+                                    },
+                                    {
+                                        content_type: "text",
+                                        title: "More Details",
+                                        payload: "More Details"
+                                    },
+                                    {
+                                        content_type: "text",
+                                        title: "Back to Menu 🔙",
+                                        payload: "filters"
+                                    }
+                                ]
+                            }
+                        });
+                        session.send(facebookMessage);
+                        break;
+                    default:
+                        session.send(messageSent)
+                        quickRepliesCard = MessagesController.addQuickRepliesButtons(quickRepliesCard, quickRepliesButtons, "Buy this product 🛒");
+                        quickRepliesCard = MessagesController.addQuickRepliesButtons(quickRepliesCard, quickRepliesButtons, "More Details");
+                        quickRepliesCard = MessagesController.addQuickRepliesButtons(quickRepliesCard, quickRepliesButtons);
+                        session.send(MessagesController.sendQuickReplies(session, quickRepliesCard, "What do you want to do ?"));
+                        break;
+                }
                 session.endDialog();
             }
         ]
