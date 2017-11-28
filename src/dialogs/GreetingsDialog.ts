5399e5c682020ceec99c4560ef33747f49147a44
Elendan <arthur.despeaux@hotmail.fr>
2017-11-28 10:52:00 +0100

    greetings

--- src/dialogs/GreetingsDialog.ts
+++ src/dialogs/GreetingsDialog.ts
@@ -11,7 +11,6 @@ class GreetingsDialog extends BaseDialog {
             (session, args, next) => {
                 let quickRepliesCard = new builder.HeroCard(session);
                 let quickRepliesButtons: builder.ICardAction[] = [];
-                session.send("Hello and welcome in the Pernod Ricard's catalog of products.");
                 quickRepliesCard.text("You can find products using the buttons below or simply typing the name of the product.");
                 quickRepliesCard = MessagesController.addQuickRepliesButtons(quickRepliesCard, quickRepliesButtons, "Brands 🍾");
                 quickRepliesCard = MessagesController.addQuickRepliesButtons(quickRepliesCard, quickRepliesButtons, "Categories 🍸");
@@ -37,6 +36,7 @@ class GreetingsDialog extends BaseDialog {
                     });
                 }
                 else {
+                    session.send("Hello and welcome in the Pernod Ricard's catalog of products.");
                     session.send(MessagesController.sendQuickReplies(session, quickRepliesCard));
                 }
                 session.endDialog();
