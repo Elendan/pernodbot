a9ad66a13c44cc69df5b644921dd942e5b5f84f7
Elendan <arthur.despeaux@hotmail.fr>
2017-11-28 09:38:51 +0100

    channel detection

--- src/dialogs/DialogflowDialog.ts
+++ src/dialogs/DialogflowDialog.ts
@@ -4,84 +4,101 @@ import BaseDialog from "./basedialog";
 
 class DialogflowDialog extends BaseDialog {
 
-    constructor() {
-        super();
-        this.dialog = [
-            (session, args, next) => {
-                let fulfillment = builder.EntityRecognizer.findEntity(args.intent.entities, "fulfillment");
-                if (fulfillment && (fulfillment.entity.messages.length)) {
-                    let messages = fulfillment.entity.messages.filter(message => {
-                        return message.platform === "facebook";
-                    });
-                    if (!messages.length) {
-                        messages = fulfillment.entity.messages;
-                    }
-                    let responseMessage = new builder.Message(session);
-                    let responseMessageAttachments: builder.AttachmentType[] = [];
-                    messages.forEach(message => {
-                        switch (message.type) {
-                            case 0:
-                                // Text
-                                responseMessage.text(message.speech);
-                                session.send(responseMessage);
-                                responseMessage = new builder.Message(session);
-                                break;
-                            case 1:
-                                // Card
-                                responseMessage.attachmentLayout(builder.AttachmentLayout.carousel);
-                                let card = new builder.HeroCard(session)
-                                    .title(message.title)
-                                    .subtitle(message.subtitle)
-                                    .text("")
-                                    .images([builder.CardImage.create(session, message.imageUrl)]);
-                                let buttons: builder.ICardAction[] = [];
-                                message.buttons.forEach(button => {
-                                    buttons.push({
-                                        type: button.postback ? "openUrl" : "postBack",
-                                        title: button.text,
-                                        value: button.postback || button.text
-                                    });
-                                });
-                                card.buttons(buttons)
-                                responseMessageAttachments.push(card);
-                                responseMessage.attachments(responseMessageAttachments);
-                                break;
-                            case 2:
-                                // Quick replies
-                                responseMessage.text(message.title);
-                                responseMessage.attachmentLayout(builder.AttachmentLayout.list);
-                                let quickRepliesCard = new builder.HeroCard(session);
-                                let quickRepliesButtons: builder.ICardAction[] = [];
-                                message.replies.forEach(replie => {
-                                    quickRepliesButtons.push({
-                                        type: "postBack",
-                                        title: replie,
-                                        value: replie
-                                    });
-                                });
-                                quickRepliesCard.buttons(quickRepliesButtons);
-                                responseMessageAttachments.push(quickRepliesCard);
-                                responseMessage.attachments(responseMessageAttachments);
-                                break;
-                            case 3:
-                                // Image
-                                responseMessage.attachmentLayout(builder.AttachmentLayout.carousel);
-                                responseMessageAttachments.push(new builder.HeroCard(session)
-                                    .images([builder.CardImage.create(session, message.imageUrl)]));
-                                responseMessage.attachments(responseMessageAttachments);
-                                break;
-                            default:
-                                break;
-                        }
-                    });
-                    if (responseMessageAttachments.length) {
-                        session.send(responseMessage);
-                    }
-                }
-                session.endDialog();
-            }
-        ]
+  private messengerResponse(session: builder.Session, messages: any[]): void {
+
+  }
+
+
+  private defaultResponse(session: builder.Session, messages: any[]): void {
+    let responseMessage = new builder.Message(session);
+    let responseMessageAttachments: builder.AttachmentType[] = [];
+    messages.forEach(message => {
+      switch (message.type) {
+        case 0:
+          // Text
+          responseMessage.text(message.speech);
+          session.send(responseMessage);
+          responseMessage = new builder.Message(session);
+          break;
+        case 1:
+          // Card
+          responseMessage.attachmentLayout(builder.AttachmentLayout.carousel);
+          let card = new builder.HeroCard(session)
+            .title(message.title)
+            .subtitle(message.subtitle)
+            .text("")
+            .images([builder.CardImage.create(session, message.imageUrl)]);
+          let buttons: builder.ICardAction[] = [];
+          message.buttons.forEach(button => {
+            buttons.push({
+              type: button.postback ? "openUrl" : "postBack",
+              title: button.text,
+              value: button.postback || button.text
+            });
+          });
+          card.buttons(buttons)
+          responseMessageAttachments.push(card);
+          responseMessage.attachments(responseMessageAttachments);
+          break;
+        case 2:
+          // Quick replies
+          responseMessage.text(message.title);
+          responseMessage.attachmentLayout(builder.AttachmentLayout.list);
+          let quickRepliesCard = new builder.HeroCard(session);
+          let quickRepliesButtons: builder.ICardAction[] = [];
+          message.replies.forEach(replie => {
+            quickRepliesButtons.push({
+              type: "postBack",
+              title: replie,
+              value: replie
+            });
+          });
+          quickRepliesCard.buttons(quickRepliesButtons);
+          responseMessageAttachments.push(quickRepliesCard);
+          responseMessage.attachments(responseMessageAttachments);
+          break;
+        case 3:
+          // Image
+          responseMessage.attachmentLayout(builder.AttachmentLayout.carousel);
+          responseMessageAttachments.push(new builder.HeroCard(session)
+            .images([builder.CardImage.create(session, message.imageUrl)]));
+          responseMessage.attachments(responseMessageAttachments);
+          break;
+        default:
+          break;
+      }
+    });
+
+    if (responseMessageAttachments.length > 0) {
+      session.send(responseMessage);
     }
+  }
+
+  constructor() {
+    super();
+    this.dialog = [
+      (session, args, next) => {
+        let fulfillment = builder.EntityRecognizer.findEntity(args.intent.entities, "fulfillment");
+        if (fulfillment && (fulfillment.entity.messages.length)) {
+          let messages = fulfillment.entity.messages.filter(message => {
+            return message.platform === "facebook";
+          });
+          if (!messages.length) {
+            messages = fulfillment.entity.messages;
+          }
+          switch (session.message.source) {
+            case "facebook":
+              this.messengerResponse(session, messages);
+              break;
+            default:
+              this.defaultResponse(session, messages);
+              break;
+          }
+        }
+        session.endDialog();
+      }
+    ]
+  }
 }
 
 export default DialogflowDialog;
