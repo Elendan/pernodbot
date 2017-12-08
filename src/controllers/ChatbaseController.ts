5100a09863a7bdb93a50d1c720027fb2d6a91390
Elendan <arthur.despeaux@hotmail.fr>
2017-12-08 16:50:54 +0100

    update key

--- src/controllers/ChatbaseController.ts
+++ src/controllers/ChatbaseController.ts
@@ -1,39 +1,52 @@
-import * as https from "https";
 import * as builder from "botbuilder";
 import * as ChatBase from "@google/chatbase";
 
 class ChatBaseController {
 
-    public static sendNotHandled(session: builder.Session, platform: string, message: string, intent: string) : any {
-        const newMsg = ChatBase.newMessage(process.env.CHATBASE_API_KEY)
-        .setAsTypeUser()
-        .setPlatform('facebook')
-        .setMessage('Hello')
-        .setVersion('1.0')
-        .setIntent('start.dialog')
-        .setAsNotHandled()
-        .setUserId(session.message.address.conversation.id)
-        .send()
-        .then(msg => console.log(msg.getCreateResponse()))
-        .catch(err => console.error(err));
+    /**
+     * Tells to ChatBase the message sent by user is not handeled
+     * @param session 
+     * @param platform 
+     * @param message 
+     * @param intent 
+     */
+    public static sendNotHandled(session: builder.Session, platform: string, message: string, intent: string): any {
+        const msg = ChatBase.newMessage(process.env.CHATBASE_API_KEY)
+            .setAsTypeUser()
+            .setPlatform('facebook')
+            .setMessage('Hello')
+            .setVersion('1.0')
+            .setIntent('start.dialog')
+            .setAsNotHandled()
+            .setUserId(session.message.address.conversation.id)
+            .send()
+            .then(msg => console.log(msg.getCreateResponse()))
+            .catch(err => console.error(err));
 
-        return newMsg;
+        return msg;
     }
 
-    public static sendHandled(session: builder.Session, platform: string, message: string, intent: string) : any {
-        const newMsg = ChatBase.newMessage(process.env.CHATBASE_API_KEY)
-        .setAsTypeUser()
-        .setPlatform(platform)
-        .setMessage(message)
-        .setVersion('1.0')
-        .setIntent(intent)
-        .setAsHandled()
-        .setUserId(session.message.address.conversation.id)
-        .send()
-        .then(msg => console.log(msg.getCreateResponse()))
-        .catch(err => console.error(err));
+    /**
+     * Tells to ChatBase the message sent by user is handeled
+     * @param session 
+     * @param platform 
+     * @param message 
+     * @param intent 
+     */
+    public static sendHandled(session: builder.Session, platform: string, message: string, intent: string): any {
+        const msg = ChatBase.newMessage(process.env.CHATBASE_API_KEY)
+            .setAsTypeUser()
+            .setPlatform(platform)
+            .setMessage(message)
+            .setVersion('1.0')
+            .setIntent(intent)
+            .setAsHandled()
+            .setUserId(session.message.address.conversation.id)
+            .send()
+            .then(msg => console.log(msg.getCreateResponse()))
+            .catch(err => console.error(err));
 
-        return newMsg;
+        return msg;
     }
 
 }
